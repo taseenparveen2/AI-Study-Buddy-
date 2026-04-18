c563d7476318df364f1875fbdc14c5817e402118
taseenparveen2 <taseenparveen43@gmail.com>
2026-04-18 10:35:27 +0530

    Added langchain-groq dependency

--- utils/llm.py
+++ utils/llm.py
@@ -1,8 +1,11 @@
 from langchain_groq import ChatGroq
 import os
+from dotenv import load_dotenv
 
-def get_llm():
+load_dotenv()
+
+def get_llm(model_name="llama-3.1-8b-instant"):
     return ChatGroq(
         groq_api_key=os.getenv("GROQ_API_KEY"),
-        model_name="llama-3.1-8b-instant"
+        model_name=model_name
     )
