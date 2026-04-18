7d3880da88ad0da2bf8ecee5ad4b3a9d3b660fb6
taseenparveen2 <taseenparveen43@gmail.com>
2026-04-18 10:54:23 +0530

    fix requirements

--- utils/llm.py
+++ utils/llm.py
@@ -1,11 +1,11 @@
-from langchain_groq import ChatGroq
+from groq import Groq
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
 def get_llm(model_name="llama-3.1-8b-instant"):
-    return ChatGroq(
+    return Groq(
         groq_api_key=os.getenv("GROQ_API_KEY"),
         model_name=model_name
     )
